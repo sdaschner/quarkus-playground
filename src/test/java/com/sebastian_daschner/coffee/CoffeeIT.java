c95980a05473769d5790960470926eb0a0e2234d
Sebastian Daschner <git@sebastian-daschner.de>
2021-06-15 07:50:20 +0200

    updated content for Quarkus 2.x

--- src/test/java/com/sebastian_daschner/coffee/CoffeeIT.java
+++ src/test/java/com/sebastian_daschner/coffee/CoffeeIT.java
@@ -3,7 +3,7 @@ package com.sebastian_daschner.coffee;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class CoffeeIT {
 
