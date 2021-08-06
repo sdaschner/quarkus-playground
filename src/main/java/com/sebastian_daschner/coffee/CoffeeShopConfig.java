7ce8ee294f0526c46393e79739c4a8741005c089
Sebastian Daschner <git@sebastian-daschner.de>
2021-08-06 06:47:01 +0200

    updated Quarkus version to 2.1.1; added list config @ConfigProperty example;

--- src/main/java/com/sebastian_daschner/coffee/CoffeeShopConfig.java
+++ src/main/java/com/sebastian_daschner/coffee/CoffeeShopConfig.java
@@ -1,7 +1,10 @@
 package com.sebastian_daschner.coffee;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import java.util.List;
 
 @ApplicationScoped
 public class CoffeeShopConfig {
@@ -9,11 +12,16 @@ public class CoffeeShopConfig {
     @Inject
     ComplexConfiguration configuration;
 
+    @ConfigProperty(name = "complex.list")
+    List<String> list;
+    
     public String getConfig() {
         System.out.println("configuration.coffee() = " + configuration.coffee());
         System.out.println("configuration.list() = " + configuration.list());
         System.out.println("configuration.someOther().coffee() = " + configuration.someOther().coffee());
         System.out.println("configuration.someOther().list() = " + configuration.someOther().list());
+
+        System.out.println("list = " + list);
 
         return configuration.coffee();
     }
