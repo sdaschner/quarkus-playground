a68b87c901cc3ed2b74345df6e6a3ef16bed35a5
Sebastian Daschner <git@sebastian-daschner.de>
2022-12-02 07:21:39 +0100

    added OpenAI examples

--- run-database.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-docker run -d --rm \
-  --name coffee-shop-db \
-  -p 5432:5432 \
-  -e POSTGRES_USER=postgres \
-  -e POSTGRES_PASSWORD=postgres \
-  postgres:15.0
